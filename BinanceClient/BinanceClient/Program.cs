f75fc7d1c618c4d8fc72662c10d0a51c5e92c142
Tetiana.Domanska <tetiana.domanska@henryschein.com>
2022-11-25 14:41:35 +0200

    'How to test lib' foundation

--- BinanceClient/BinanceClient/Program.cs
+++ BinanceClient/BinanceClient/Program.cs
@@ -1,10 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using BinanceClient;
 using CryptoClientLib;
 using CryptoClientLib.Commons;
 
-var publicKey = "";
+ICryptoCurrencyCreator cryptoClientCreator = new BinanceClientCreator();
 
-var privateKey = "";
-BinanceClient client = new BinanceClient(publicKey, privateKey);
-var t = client.GetPrice(new Symbol(Asset.BTC, Asset.USDT));
+// Use your own public and private keys instead of empty strings
+// Never push them to repo 
+string publicKey = string.Empty;
+string privateKey = string.Empty;
+
+var cryptoClient = cryptoClientCreator.CreateCryptoClient(publicKey, privateKey);
+
+
+// Examples of using methods from lib below
+
+//var price = cryptoClient.GetPrice(new Symbol(Asset.BTC, Asset.USDT));
+//var createOrder = cryptoClient.CreateOrder(OrderType.LIMIT, new Symbol(Asset.BTC, Asset.USDT), Side.BUY);
+var klines = cryptoClient.GetKlines(new Symbol(Asset.BTC, Asset.USDT), "15m", 2000);
+
+// You can debug and track asyncronous processes with using of variable and loop below
+// START!!! ONLY FOR DEBUGGING
+int i = 0;
+
+while (true)
+{
+	i++;
+}
+
+// END!!! ONLY FOR DEBUGGING
